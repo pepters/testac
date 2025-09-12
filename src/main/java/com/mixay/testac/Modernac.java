026fdfc7fce41e21cfbfcb62b8bbaf87ed24764d
mixay <pk020311@gmail.com>
2025-09-12 15:26:20 +0300

    Initial commit

---
+++ src/main/java/com/mixay/testac/Modernac.java
@@ -0,0 +1,17 @@
+package com.mixay.testac;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Modernac extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
